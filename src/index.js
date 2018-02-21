602288cf1c1b3ec21aa03e3d3c1ac5e183bb119b
Kkarelia <iamcaver@gmail.com>
2018-02-21 13:21:58 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 import React from 'react';
@@ -6,77 +5,66 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-
 class Square extends React.Component {
- render() {
-   return (
-     <button className="square">
-       {/* TODO */}
-     </button>
-   );
- }
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
 }
 
 class Board extends React.Component {
- renderSquare(i) {
-   return <Square />;
- }
+  renderSquare(i) {
+    return <Square />;
+  }
 
- render() {
-   const status = 'Next player: X';
+  render() {
+    const status = 'Next player: X';
 
-   return (
-     <div>
-       <div className="status">{status}</div>
-       <div className="board-row">
-         {this.renderSquare(0)}
-         {this.renderSquare(1)}
-         {this.renderSquare(2)}
-       </div>
-       <div className="board-row">
-         {this.renderSquare(3)}
-         {this.renderSquare(4)}
-         {this.renderSquare(5)}
-       </div>
-       <div className="board-row">
-         {this.renderSquare(6)}
-         {this.renderSquare(7)}
-         {this.renderSquare(8)}
-       </div>
-     </div>
-   );
- }
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
 }
 
 class Game extends React.Component {
- render() {
-   return (
-     <div className="game">
-       <div className="game-board">
-         <Board />
-       </div>
-       <div className="game-info">
-         <div>{/* status */}</div>
-         <ol>{/* TODO */}</ol>
-       </div>
-     </div>
-   );
- }
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
 }
 
 // ========================================
 
 ReactDOM.render(
- <Game />,
- document.getElementById('root')
+  <Game />,
+  document.getElementById('root')
 );
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
->>>>>>> 8b58a3c2a1346d390da88df738dbf895b8d71061
