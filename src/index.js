615be61fec384905a441aa43649c72dd6d8a7f3c
Kkarelia <iamcaver@gmail.com>
2018-02-22 08:54:23 +0200

    dom direct

--- src/index.js
+++ src/index.js
@@ -5,66 +5,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
-  }
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
 
-  render() {
-    const status = 'Next player: X';
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-// ========================================
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
 
 ReactDOM.render(
-  <Game />,
+  element,
   document.getElementById('root')
 );
