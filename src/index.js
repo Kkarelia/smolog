fcce83a9d498cf8a6c97396e4f3736e40927311c
Kkarelia <iamcaver@gmail.com>
2018-02-22 15:49:57 +0200

    Class and form

--- src/index.js
+++ src/index.js
@@ -4,29 +4,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Smoke {
+  constructor(brand,blend,comment) {
+    this.brand = brand;
+    this.blend = blend;
+    this.timenday = new Date();
+    this.comment = comment
+  }
 
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
+  showSmoke() {
+    alert(this.brand + ' ' + this.blend)
+  }
 }
 
-function formatFirstname(user) {
-  return user.firstName;
-}
 
-function formatLastname(user) {
-  return user.lastName;
-}
 
-const user = {
-  firstName: 'Harper',
-  lastName: 'Perez'
-};
+const smoke = new Smoke("Peterson","Old Dublin", "Avesome evening smoke in vintage Custom-Bilt pipe");
 
 const element = (
   <form>
-   First Name: <input type="text" name="firstname" value={formatFirstname(user)}></input>
-   <br /> <br />
-   Last Name: <input type="text" name="lastname" value={formatLastname(user)}></input>
+    <table border="0">
+    <tr><td>Brand:</td><td><input type="text" name="brand" size="40" value={smoke.brand}></input></td></tr>
+    <tr><td>Blend:</td><td><input type="text" name="blend" size="40" value={smoke.blend}></input></td></tr>
+    <tr><td>Time:</td><td><input type="text" name="time" size="40" value={smoke.timenday}></input></td></tr>
+    <tr><td valign="top">Note:</td><td><textarea rows="4" cols="39">{smoke.comment}</textarea></td></tr>
+    </table>
   </form>
 );
 
