c486524ac4c0758fc5800705e95d908b8f98ffaf
Kkarelia <iamcaver@gmail.com>
2018-03-01 13:13:56 +0200

    Time added

--- src/index.js
+++ src/index.js
@@ -26,19 +26,26 @@ class SmokeForm extends React.Component {
       var d = new Date(),
       month = '' + (d.getMonth() + 1),
       day = '' + d.getDate(),
-      year = d.getFullYear();
+      year = d.getFullYear(),
+      hour = '' + d.getHours(),
+      minute = '' + d.getMinutes();
 
       if (month.length < 2) month = '0' + month;
       if (day.length < 2) day = '0' + day;
 
+      if (hour.length < 2) hour = '0' + hour;
+      if (minute.length < 2) minute = '0' + minute;
+
       var datestring = [year, month, day].join('-');
+      var timestring = [hour, minute].join(':');
 
 
-     this.state = {brand: 'Peterson', blend: 'Old Dublin', timenday: datestring, comment: 'Your average good smoke'};
+     this.state = {brand: 'Peterson', blend: 'Old Dublin', timenday: datestring, timeofday: timestring, comment: 'Your average good smoke'};
 
      this.brandChange = this.brandChange.bind(this);
      this.blendChange = this.blendChange.bind(this);
      this.timendayChange = this.timendayChange.bind(this);
+     this.timeofdayChange = this.timeofdayChange.bind(this);
      this.commentChange = this.commentChange.bind(this);
      this.handleSubmit = this.handleSubmit.bind(this);
    }
@@ -58,6 +65,12 @@ class SmokeForm extends React.Component {
 
   }
 
+
+     timeofdayChange(event) {
+      this.setState({timeofday: event.target.value});
+
+    }
+
   commentChange(event) {
    this.setState({comment: event.target.value});
  }
@@ -66,7 +79,7 @@ class SmokeForm extends React.Component {
 
 
    handleSubmit(event) {
-    alert('Something changed!: ' + this.state.brand  + this.state.blend  + this.state.timenday  + this.state.comment);
+    alert('Something changed!: ' + this.state.brand + ' '  + this.state.blend + ' '  + this.state.timenday + ' ' + this.state.timeofday + ' '  + this.state.comment);
     event.preventDefault();
   }
   render() {
@@ -76,6 +89,7 @@ class SmokeForm extends React.Component {
       <tr><td>Brand:</td><td><input type="text" name="brand" size="50" value={this.state.brand} onChange={this.brandChange} ></input></td></tr>
       <tr><td>Blend:</td><td><input type="text" name="blend" size="50" value={this.state.blend} onChange={this.blendChange}></input></td></tr>
       <tr><td>Date:</td><td><input type="date" name="time" size="50" value={this.state.timenday} onChange={this.timendayChange}></input></td></tr>
+      <tr><td>Time:</td><td><input type="time" name="timeofday" size="50" value={this.state.timeofday} onChange={this.timeofdayChange}></input></td></tr>
       <tr><td valign="top">Note:</td><td><textarea rows="4" cols="46" onChange={this.commentChange}>{this.state.comment}</textarea></td></tr>
       <tr><td valign="top"></td><td><input type="submit" value="Log This Smoke" /></td></tr>
       </table>
